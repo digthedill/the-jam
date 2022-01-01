ce1e4ea68f0f4352540ff3e36019d2386fc4a35d
digthedill <popssly@gmail.com>
2022-01-01 14:37:10 -0600

    inputs binded to synth build

--- src/components/TheSynth.vue
+++ src/components/TheSynth.vue
@@ -1,4 +1,65 @@
 <template>
+
+  <div class="envelope">
+    <label for="oscillator">oscillator</label>
+    <select
+      name="oscillator"
+      id="oscillator"
+      v-model="oscillator"
+      @change="buildSynth"
+    >
+      <option
+        v-for="osc in oscillatorType"
+        :key="osc"
+        :value="osc"
+      >
+        {{osc}}
+      </option>
+    </select>
+
+    <label for="attack">attack</label>
+
+    <input
+      type="range"
+      name="attack"
+      id="attack"
+      min="0"
+      max="100"
+      v-model="envelope.attack"
+      @change="buildSynth"
+    >
+    <label for="decay">decay</label>
+    <input
+      type="range"
+      name="decay"
+      id="decay"
+      min="0"
+      max="100"
+      v-model="envelope.decay"
+      @change="buildSynth"
+    >
+    <label for="sustain">sustain</label>
+    <input
+      type="range"
+      name="sustain"
+      id="sustain"
+      min="0"
+      max="100"
+      v-model="envelope.sustain"
+      @change="buildSynth"
+    >
+    <label for="release">release</label>
+    <input
+      type="range"
+      name="release"
+      id="release"
+      min="0"
+      max="100"
+      v-model="envelope.release"
+      @change="buildSynth"
+    >
+
+  </div>
   <the-key
     v-for="(note, i) in notes"
     :key="`${note}${i}`"
@@ -6,6 +67,7 @@
     :octave="octave"
     @click-note="handleClick"
   />
+
 </template>
 
 <script>
@@ -22,6 +84,14 @@ export default {
       dictionary,
       octave: 4,
       notes: Object.values(dictionary),
+      envelope: {
+        attack: 10,
+        decay: 20,
+        sustain: 50,
+        release: 80,
+      },
+      oscillatorType: ['amsine', 'amsquare', 'amsawtooth', 'amtriangle'],
+      oscillator: 'amsine',
     };
   },
   mounted() {
@@ -30,9 +100,7 @@ export default {
     });
   },
   created() {
-    this.synth = new Synth({
-      volume: 0.5,
-    }).toDestination();
+    this.buildSynth();
   },
   methods: {
     handleKey(e) {
@@ -53,8 +121,38 @@ export default {
       this.$emit('setCurrentNote', note);
       this.synth.triggerAttackRelease(note + this.octave, '8n');
     },
+    buildSynth() {
+      const envelope = Object.keys(this.envelope).reduce((acc, key) => {
+        acc[key] = this.envelope[key] / 100;
+        return acc;
+      }, {});
+
+      this.synth = new Synth({
+        oscillator: {
+          partialCount: 0,
+          partials: [],
+          phase: 0,
+          type: this.oscillator,
+          harmonicity: 0.5,
+          modulationType: 'triangle',
+        },
+        envelope,
+      }).toDestination();
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.container {
+  max-width: 600px;
+  margin: auto;
+}
+.envelope {
+  display: flex;
+  flex-direction: column;
+  & > * {
+    width: 200px;
+  }
+}
+</style>
